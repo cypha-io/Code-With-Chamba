ce9d2bc67abd12ef741d34cf22c9fd7ef8643a2a
Chamba Aziel Nanang <123768840+AzielChamba@users.noreply.github.com>
2023-01-30 10:22:07 +0000

    Minor updates

--- name & password input.py
+++ name & password input.py
@@ -15,7 +15,18 @@ if (choice == 'register' or 'Register'):
         print('Loading....')
         print('Welcome,', name)
         print('Your password is:', password)
+
+        balance = 1000
+        opt_1 = balance
+        opt_2 = 'Your registered name is', name
+        print('1. Check Balance')
+        print('2. Check Name')
+        option = int(input('Select your option\n'))
+        if (option == 1):
+            print('Your balance is', opt_1)
+        else:
+            print(opt_2)
     else:
         print('Invalid name or passoword')
-
-
+else:
+    print('Kindly try again soon...')
