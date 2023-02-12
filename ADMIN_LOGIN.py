033908efb9a4e55600276592c5cf4d220630ee02
Chamba Aziel Nanang <123768840+AzielChamba@users.noreply.github.com>
2023-02-12 12:19:10 +0000

    updates

--- ADMIN_LOGIN.py
+++ ADMIN_LOGIN.py
@@ -4,6 +4,7 @@ import hashlib
 print('Follow the instructions below carefully')
 choice = input('Admin Or User\n')
 
+
 #Code to check if person is entering as a user or an admin
 if (choice == 'Admin' or 'admin'):
     name = input('Enter your name:\n')
@@ -14,19 +15,24 @@ if (choice == 'Admin' or 'admin'):
     if (name == 'chamba' or name == 'Chamba' and password == stored_password):
         print('Loading....')
         print('Welcome,', name)
-        print('Your password is:', password)
 
-        #change this to fit Assigned Teammates tomorrow
-        balance = 1000
-        opt_1 = balance
+#change this to fit Assigned Teammates tomorrow
+        ass_wrk = 'You have no assigned work'
+        opt_1 = ass_wrk
         opt_2 =  name
+    while True:    
         print('1. Check Assigned Work')
         print('2. Check Team Members')
+        print('3. Exit')
         option = int(input('Select your option\n'))
         if (option == 1):
-            print('Your balance is', opt_1)
+            print(opt_1)
+            print('----------------------------------------')
         elif(option == 2):
             print('Registered name is', opt_2)
+            print('----------------------------------------')
+        elif(option == 3):
+            break
         else:
             print('Error!')
     else:
