ce134436e72b050272b91bf6d67ec1412adc23c5
Chamba Aziel Nanang <123768840+AzielChamba@users.noreply.github.com>
2023-02-24 07:32:56 +0000

    Fixed bug and updates for login failed

--- cypha_systems_login.py
+++ cypha_systems_login.py
@@ -6,6 +6,8 @@ import hashlib
 print('Welcome To Cypha Python Systems')
 print('Please Wait...')
 time.sleep(3)
+ass_wrk = 'You have no assigned work'
+opt_2 =  'No available members. Thank you.'
 
 choice = input('Login As: ')
 
@@ -16,42 +18,45 @@ if (choice == 'Admin' or 'admin'):
     password_input = hashlib.sha256(password.encode()).hexdigest()
     stored_password = 'admin123'
 
-    if (password != stored_password):
-        print('Error 403')
+
+
+
+
+
+
     if (name == 'chamba' or name == 'nartey'  and password == stored_password):
         print('Loading....')
         time.sleep(2)
         print('Welcome,', name)
-    ass_wrk = 'You have no assigned work'
-    opt_2 =  'No available members. Thank you.'
-    while True:    
-        print('1. Check Assigned Work')
-        print('2. Check Team Members')
-        print('3. Password')
-        print('4. Exit')
-        option = int(input('Select your option: '))
-        if (option == 1):
-            time.sleep(3)
-            print(ass_wrk)
-            print('----------------------------------------')
-        elif(option == 2):
-            time.sleep(3)
-            print(opt_2)
-            print('----------------------------------------')
-        elif(option == 3):
-            time.sleep(3)
-            print('1. Check Password')
-            print('2. Reset Password')
-            pass_ = int(input('Select your option: '))
-            if(pass_ == 1):
+        while True:    
+            print('1. Check Assigned Work')
+            print('2. Check Team Members')
+            print('3. Password')
+            print('4. Exit')
+            option = int(input('Select your option: '))
+            if (option == 1):
                 time.sleep(3)
-                print('Your current password is:', password)
-            
-        elif(option == 4):
-            print('Exiting Please Wait....')
-            time.sleep(5)
-            break
-        else:
-            print('Error!')
+                print(ass_wrk)
+                print('----------------------------------------')
+            elif(option == 2):
+                time.sleep(3)
+                print(opt_2)
+                print('----------------------------------------')
+            elif(option == 3):
+                time.sleep(3)
+                print('1. Check Password')
+                print('2. Reset Password')
+                pass_ = int(input('Select your option: '))
+                if(pass_ == 1):
+                    time.sleep(3)
+                    print('Your current password is:', password)
+                
+            elif(option == 4):
+                print('Exiting Please Wait....')
+                time.sleep(5)
+                break
+            else:
+                print('Error!')
     else:
-        print('Invalid name or passoword')
+        print('invalid')
+    
