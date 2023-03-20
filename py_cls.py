f96f9c025328c2af0176690f1a0213cb74bbaf46
Chamba Aziel Nanang <123768840+AzielChamba@users.noreply.github.com>
2023-03-20 14:19:02 +0000

    Done

--- py_cls.py
+++ py_cls.py
@@ -1,1 +1,12 @@
-chamba
+x = "THE FUN PART OF PROGRAMMING IS WRITING SYNTAX"
+
+lenght = len(x)
+
+print("The length is ", lenght)
+
+py = 22/7
+r = lenght*lenght
+
+area = py * r
+
+print("The Area Is",area)
