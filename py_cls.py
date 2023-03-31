5293b46a8054e72e5bb914953ffc80227f6bbee8
Chamba Aziel Nanang <123768840+AzielChamba@users.noreply.github.com>
2023-03-31 14:25:25 +0000

    done

--- py_cls.py
+++ py_cls.py
@@ -1,39 +1,7 @@
-<<<<<<< HEAD
-x = "THE FUN PART OF PROGRAMMING IS WRITING SYNTAX"
-
-lenght = len(x)
-
-print("The length is ", lenght)
-
+word = "OLAG is an amazing SHS to attend"
 py = 22/7
-r = lenght*lenght
+radius = len(word)
+r_sqr = radius^2
+area = 4 * py * r_sqr
 
-area = py * r
-
-print("The Area Is",area)
-
-
-#Number 2
-import math
-
-A = float(input("Enter the value of A: "))
-b = float(input("Enter the value of B: "))
-c = float(input("Enter the value of C: "))
-
-discriminant = b**2 - 4*a*c
-
-if discriminant < 0:
-    print('It should be more than 0')
-else:
-
-    x1 = (-b + math.sqrt(discriminant)) / (2*a)
-    x2 = (-b - math.sqrt(discriminant)) / (2*a)
-
-    print("The solutions are:")
-    print("x1 =", x1)
-    print("x2 =", x2)
-
-
-=======
->>>>>>> e09541a59d724bc60a7859989337f39848dd6548
-
+print(area)
