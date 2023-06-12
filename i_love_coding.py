0f7d6a2571757d16c8ca2e3f63a8665717324fed
cyphahimself <123768840+cypha-io@users.noreply.github.com>
2023-06-12 14:35:22 +0000

    Update i_love_coding.py

--- i_love_coding.py
+++ i_love_coding.py
@@ -1,1 +1,22 @@
-list ['chamba', 'chamba', 3]
+user = input('Enter a letter: ')
+
+vowels = ['a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I']
+consonansts = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y','z']
+
+if (user in vowels):
+    print('This is a vowel')
+    user_num_1 = int(input('Enter a number: '))
+    if (user_num_1 % 2) == 0:
+        print('This is an even number')
+    else:
+        print('This is an odd number')
+elif (user in consonansts):
+    print('This is a consonansts')
+    user_num_2 = int(input('Enter a number: '))
+    if (user_num_2 < 0):
+        print('The number is a negative number')
+    else:
+        print('The number is a positive number')
+else: 
+    print('Error 404!')
+    
